37c8c37c56b5e24d732798d93aecb9baf4b70a58
Marcelo Bartsch <marcelo@bartsch.cl>
2018-03-21 12:11:58 +0000

    If user already exists, just ignore the error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -242,13 +242,9 @@ if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_strin
 if ($usingdbroot) {
   if (!$mysql->query('CREATE USER `' . $mysql->real_escape_string(getenv('WORDPRESS_DB_USER')) . '`@`%` IDENTIFIED BY "'. $mysql->real_escape_string(getenv('WORDPRESS_DB_PASSWORD')) .'"')) {
     fwrite($stderr, "\n" . 'MySQL "CREATE USER" Error: ' . $mysql->error . "\n");
-    $mysql->close();
-    exit(1);
   }
   if (!$mysql->query('GRANT ALL ON ' . $mysql->real_escape_string($dbName) . '.* TO `' . $mysql->real_escape_string($user)  . '`@`%`')) {
     fwrite($stderr, "\n" . 'MySQL "GRANT ALL" Error: ' . $mysql->error . "\n");
-    $mysql->close();
-    exit(1);
   }
 }
 
