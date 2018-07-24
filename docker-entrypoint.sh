d4ab49479b1441b990fa0aacc15b5655ac7afc78
Marcelo Bartsch <marcelo@bartsch.cl>
2018-07-24 22:51:52 +0100

    add extra-php

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -120,10 +120,14 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 	# version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
 	# https://github.com/docker-library/wordpress/issues/116
 	# https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
-	sed -ri -e 's/\r$//' wp-config*
 
-	if [ -e wp-config.php ]; then
-			awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
+	sudo -u wp-admin -i -- wp config create \
+		--dbname=${WORDPRESS_DB_NAME:=wordpress} \
+		--dbuser="${WORDPRESS_DB_USER:=root}" \
+		--dbpass="${WORDPRESS_DB_PASSWORD:=}" \
+		--dbhost="${WORDPRESS_DB_HOST:=mysql}" \
+		--dbprefix="${WORDPRESS_TABLE_PREFIX:=wp_}" \
+		--skip-salts --force --extra-php <<PHP
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
@@ -134,17 +138,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 	define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
 	define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
 }
-EOPHP
-		chown "$user:$group" wp-config.php
-	fi
-
-	sudo -u wp-admin -i -- wp config create \
-		--dbname=${WORDPRESS_DB_NAME:=wordpress} \
-		--dbuser="${WORDPRESS_DB_USER:=root}" \
-		--dbpass="${WORDPRESS_DB_PASSWORD:=}" \
-		--dbhost="${WORDPRESS_DB_HOST:=mysql}" \
-		--dbprefix="${WORDPRESS_TABLE_PREFIX:=wp_}" \
-		--skip-salts --force
+PHP
 
 	if [ "$WORDPRESS_DEBUG" ]; then
 		sudo -u wp-admin -i -- wp config set WP_DEBUG true --raw --type=constant
